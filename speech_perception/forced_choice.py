22c4d56b3ca056e8897e96d9cde8bfc3be6d030b
Maria Duong <maria.duong.007@gmail.com>
2020-02-16 19:10:26 -0500

    Complete forced_choice optionalities.
    Original code is still spaghetti-like.

--- speech_perception/forced_choice.py
+++ speech_perception/forced_choice.py
@@ -25,6 +25,9 @@ prefs.general['audioLib'] = ['pygame']
 from psychopy import sound
 import random, time, sys, os
 
+global trial, block
+
+
 ##### USER INPUT #####
 
 if sys.platform[:3] == "win":
@@ -39,8 +42,10 @@ choice1_text = 'pa'
 choice2_text = 'ba'
 
 # these are the key options that participants should press (left/right)
-key1 = 'f'
-key2 = 'j'
+# key1 = 'f'
+# key2 = 'j'
+
+input = 'mouse'
 
 # iti = inter-trial interval, the duration of time between trials
 iti = 0.5
@@ -69,6 +74,7 @@ dataFile_cat.write('sub\ttrial\tfile\tchoice\trt\n')
 
 reps = int(exp_info['Repetitions'])
 
+
 ##### CATEGORIZATION #####
 def categorization(stimdir, block):
     global trial
@@ -83,54 +89,135 @@ def categorization(stimdir, block):
     num_stim = len(stim)
     count = 1
     message.setText("Block " + str(block))
-    message2 = visual.TextStim(win, pos=(0,-.2), wrapWidth=1.3, height=0.1, color=-1)
+    message.pos = (0.7, -0.2)
+    message2 = visual.TextStim(win, pos=(0.7, -0.4), wrapWidth=1.3, height=0.1, color=-1)
     message2.setText("Press any key to begin")
     message2.setAutoDraw(True)
     win.flip()
-    event.waitKeys()    
+    event.waitKeys()
     random.shuffle(stim)
+
+    # for element in screen_elements:
+    #      element.setAutoDraw(True)
+
+    message.setText('')
+    win.flip()
     for word in stim:
-        if flag == 1:
-            flag = 0
-            break
-        message.setText(choice1_text + "         -----         " + choice2_text)
+
+        # if flag == 1:
+        #     flag = 0
+        #     break
+        # message.setText(choice1_text + "         -----         " + choice2_text)
         message2.setText("Block "+ str(block) + "-- Trial " + str(count) + " of " + str(len(stim)))
         win.flip()
+        screen_elements['bshape'].draw()
+        screen_elements['bword'].draw()
+        screen_elements['pshape'].draw()
+        screen_elements['pword'].draw()
+        win.flip()
 
+        choice = ''
         wav = sound.Sound(value=stimdir+word, sampleRate=44100)
         rt_clock.reset()
+        dur = wav.getDuration()
         wav.play()
-        keys = event.waitKeys(keyList = [key1,key2,'escape'], timeStamped=rt_clock)
-        
-        # rt = "response time" 
-        rt = keys[0][1]
-        choice = keys[0][0]
-        if choice =="escape":
-            core.quit()
-        dataFile_cat.write(exp_info['Participant'] + '\t' + str(trial) + '\t' + word+'\t' +choice+'\t'+str(rt)+'\n')
+        core.wait(dur)
+        # keys = event.waitKeys(keyList=['escape'], timeStamped=rt_clock)
+
+        # pressed = event.waitKeys()
+        # if "escape" in pressed:
+        #     core.quit()
+        if input == 'mouse':
+            # key = "0"
+            while choice == '':
+                # in_shape = 0
+                allKeys = event.getKeys(keyList=['escape'])
+                for key in allKeys:
+                    if "escape" in key:
+                        core.quit()
+                mouse.getPos()
+                mouseClock.reset()
+                if mouse.getPressed()[0] == 1:
+                    rt = mouseClock.getTime()
+                    if bshape.contains(mouse):
+                        choice = 'ba'
+                    elif pshape.contains(mouse):
+                        choice = 'pa'
+        screen_elements['bshape'].draw()
+        screen_elements['bword'].draw()
+        screen_elements['pshape'].draw()
+        screen_elements['pword'].draw()
+        win.flip()
+
+
+
+        rt = mouseClock.getTime()
+        # rt = keys[0][1]
+        # choice = keys[0][0]
+        # if choice =="escape":
+        #     core.quit()
+        dataFile_cat.write(exp_info['Participant'] + '\t' + str(trial) + '\t' + word+'\t' + choice +'\t'+str(rt)+'\n')
         core.wait(iti)
         count += 1
         trial += 1
     message2.setText("")
     message.setText("")
     win.flip()
+    block += 1
+
+# global block
+# global trial
 
 if exp_info['FullScr']=="T":
     win = visual.Window(fullscr=True, color = 1)
 else:
     win = visual.Window(size=(1000, 600), color = 1)
-message = visual.TextStim(win, wrapWidth=1.3, height=0.1, color=-1)
+
+mouse = event.Mouse()
+mouse.getPos()
+mouseClock = core.Clock()
+
+bshape = visual.Rect(win=win, name='bshape',
+                     width=[0.5, 0.7][0], height=[0.4, 0.4][1],
+                     pos=[-0.4, 0.2], lineWidth=1, lineColor=-1, opacity=.2)
+bword = visual.TextStim(win, pos=(0.5, 0.2), height=0.2, color=-1)
+bword.setText('ba')
+
+
+pshape = visual.Rect(win=win, name='pshape',
+                     width=[0.5, 0.5][0], height=[0.4, 0.4][1],
+                     pos=[0.2, 0.2], lineWidth=1, lineColor=-1, opacity=.2)
+pword = visual.TextStim(win, pos=(1.1, 0.2), height=0.2, color=-1)
+pword.setText('pa')
+
+# screen_elements = [bshape, pshape, bword, pword]
+screen_elements = {'bshape': bshape, 'pshape': pshape, 'bword': bword, 'pword': pword}
+
+message = visual.TextStim(win, wrapWidth=1.3, height=0.1, color=-1, pos=(0.7, 0))
 message.setAutoDraw(True)
-message.setText("Please press '" +key1+ "' or '" +key2+ "' to indicate what you heard.")
+# message.setText("Please press " + key1 + " or " + key2 + " to indicate what you heard.")
+message.setText("Please click " + choice1_text + " or " + choice2_text + " to indicate what you heard.")
 win.flip()
 event.waitKeys()
 
 global block
 global trial
 trial = 1
-for i in range(1, reps+1):
+for i in range(1, reps + 1):
     categorization(stimdir, i)
 
 message.setText("Thank you!")
 win.flip()
 event.waitKeys()
+
+# message.setAutoDraw(False)
+# win.flip()
+# message.setText("Thank you!")
+# message.draw()
+# win.flip()
+# event.waitKeys()
+
+
+
+
+
